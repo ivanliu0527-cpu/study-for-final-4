2e9f3844891931f1461a75459e252b24d4204027
ivanliu0527-cpu <ivanliu0527@gmail.com>
2025-12-17 11:42:36 +0800

    Create a business dashboard using Streamlit

---
+++ Layouts.py
@@ -0,0 +1,65 @@
+import streamlit as st
+import time
+
+st.title("Business Dashboard with Streamlit Layouts")
+st.write(""Objective: To demonstrate the usage of columns, tabs, and dynamic containers in a business dashboard.")
+
+col1, col2, col3, = st.columns(3)
+with col1:
+  st.header("Q1 2024")
+  st.write("Revenue: $1.2M")
+with col2:
+  st.header("Q2 2024")
+  st.write("Revenue: $1.5M")
+with col3:
+  st.header("Q3 2024")
+  st.write("Revenue: $1.3M")
+
+tab1, tab2, tab3 = st.tabs(["Sales Data","Customer Insight", "Market Trends"])
+with tab1:
+  st.write("Content for Sales Data")
+  sales_data = {"Q1 2024": "$1.2M",
+        "Q2 2024": "$1.5M",
+        "Q3 2024": "$1.3M",
+        "Q4 2024": "$1.6M"
+    }
+  for quarter, revenue in sales_data.item():
+    st.write(f"{quarter}:{revenue}")
+with tab2:
+  st.write(""Content for Customer Insights")
+  customer_feedback = ["Great service!",
+        "Very satisfied with the product quality.",
+        "Quick delivery and excellent support."
+    ]
+  for feedback in customer_feedback:
+    st.write(f"- {feedback}")
+with tab3:
+  st.write("Content for Market Trends")
+  market_tend = {
+    "Eco-friendly product": " Increasing demand",
+    "Online shopping": "Continued growth",
+    "Subscription services": "Rising popularity"
+    }
+  for trend, status in market_tend.item():
+    st.write(f"{tend}: {status}")
+
+with st.expander("More Information")
+  st.write("Additional details on data collection methods.")
+  st.write("Data was collected through surveys and sales reports.")
+
+placeholder = st.empty()
+
+for i in range(5):
+  placeholder.write(f"Loading data... {i*20}% complete")
+  time.sleep(1)
+
+placeholder.write("Data loading complete. Displaying business insights.")
+
+business_insights = [
+    "Revenue increased by 15% in Q1 2024.",
+    "Customer satisfaction improved by 10%.",
+    "Market trends show a growing demand for eco-friendly products."
+]
+for insight in business_insights:
+  placeholder.write(insight)
+  time.sleep(2)
